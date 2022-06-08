8815d8c3296698578ae16581e8038c08e69e71e4
AlaeldinMusa91 <alaeldinmusa91@gmail.com>
2022-06-08 14:27:24 -0700

    update7

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Database\Eloquent\Model;
 
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -42,7 +41,7 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-    public function resturant(){
-        return $this->hasMany(Resrurant::class);
-    }
+  public  function resturants(){
+      return $this->hasMany(Resturant::class,'owner_id');
+  }
 }
